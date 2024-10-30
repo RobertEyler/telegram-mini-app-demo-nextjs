3dee6d71bbf4cd05e6277a3303312ccf16d83e15
you <you@example.com>
2024-10-30 21:09:10 +0800

    first commit

---
+++ src/app/okx/okxProvider.tsx
@@ -0,0 +1,46 @@
+import {createContext, ReactNode, useContext, useEffect, useState} from "react";
+import {OKXUniversalProvider} from "@okxconnect/universal-provider";
+import WebApp from "@twa-dev/sdk";
+
+const OkxContext = createContext<OKXUniversalProvider | null>(null);
+
+type OkxProviderProps = {
+    opts: {
+        dappMetaData: {
+            name: string;
+            /**
+             * URL to the dapp icon. Must be PNG, ICO, ... . SVG icons are not supported.
+             * @default best quality favicon declared via <link> in the document or '' if there are no any icons in the document.
+             */
+            icon: string;
+        }
+
+    },
+    children:ReactNode
+
+}
+
+const OkxProvider = (props: OkxProviderProps) => {
+
+    const [oKXUniversalProvider, setOKXUniversalProvider] = useState<OKXUniversalProvider | null>(null);
+    useEffect(() => {
+        OKXUniversalProvider.init(props.opts).then(res => setOKXUniversalProvider(res)).catch(err=>console.log(err));
+    }, [props.opts]);
+    WebApp.onEvent("viewportChanged",()=>{
+        OKXUniversalProvider.init(props.opts).then(res => setOKXUniversalProvider(res)).catch(err=>console.log(err));
+    })
+    return (
+        <OkxContext.Provider value={oKXUniversalProvider}>
+            {
+                props.children
+            }
+        </OkxContext.Provider>
+    );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useOkx = ():OKXUniversalProvider|null=>{
+    return useContext<OKXUniversalProvider|null>(OkxContext);
+}
+
+export default OkxProvider;
